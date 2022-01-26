cbb6e2966e764ba57dafc31f5b1a7e5d4385451b
Alejandra Bastidas <bastidasaleja@hotmail.com>
2022-01-26 12:48:51 -0500

    Configuracion mapa y marker

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import Input from '../../shared/components/FormElements/Input'
+import './NewPlace.css'
+
 const NewPlace = () => {
     return (
-        <div>
-            Page New Place
-        </div>
+        <form className='place-form'>
+            <Input element='input' type='text' label='Title'/>
+      
+        </form>
     )
 }
 
